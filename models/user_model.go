d28cd4659f20f28560c5385d5b36e785133ba096
kamah-01 <millerbeautyagencies@gmail.com>
2024-06-07 19:43:18 +0300

    user model

--- models/user_model.go
+++ models/user_model.go
@@ -6,12 +6,15 @@ import (
 
 // User struct to define a user model
 type User struct {
-    ID        string    `json:"id"`
-    Username  string    `json:"username" bson:"username" validate:"required,min=3,max=20"`
-    Name      string    `json:"name" bson:"name" validate:"required"`
-    Email     string    `json:"email" bson:"email" validate:"required,email"`
-    Password  string    `json:"password" bson:"password" validate:"required,min=8"`
-	Role      string    `json:"role" bson:"role" validate:"required,min=8"`
-    CreatedAt time.Time `json:"created_at" bson:"created_at"`
-    UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
+    ID               string    `json:"id"`
+    Username         string    `json:"username" bson:"username" validate:"required,min=3,max=20"`
+    Name             string    `json:"name" bson:"name" validate:"required"`
+    Email            string    `json:"email" bson:"email" validate:"required,email"`
+	Phone            string    `json:"phone" bson:"phone" validate:"required,phone"`
+	ImageProfile     *string    `json:"image_profile" bson:"image_profile"`
+	Bio              string    `json:"bio" bson:"bio" validate:"required"`
+    Password         string    `json:"password" bson:"password" validate:"required,min=8"`
+	Role             string    `json:"role" bson:"role" validate:"required,min=8"`
+    CreatedAt        time.Time `json:"created_at" bson:"created_at"`
+    UpdatedAt        time.Time `json:"updated_at" bson:"updated_at"`
 }
