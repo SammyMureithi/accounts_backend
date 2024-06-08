30cf41929a1f0ae7942172edabc9b49356252d53
kamah-01 <millerbeautyagencies@gmail.com>
2024-06-08 13:20:18 +0300

    admin routes

--- routes/admin_routes.go
+++ routes/admin_routes.go
@@ -10,25 +10,75 @@ import (
 )
 
 func AdminRoutes(router *mux.Router) {
-	//Admin Routes 
+	//Admin Specific Routes 
 	approveEntryChangeRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(controllers.ApproveEntryChange), []string{"Admin"})
     router.Handle("/admin/entry/{entryId}", approveEntryChangeRoute).Methods("PUT")
-	getAllEntriesRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(controllers.GetAllEntries), []string{"Admin"})
-    router.Handle("/admin/entry", getAllEntriesRoute).Methods("GET")
+
+	//Admin Specific
 	getEntriesRequestingChanges:=middleware.RoleBasedJWTMiddleware(http.HandlerFunc(controllers.GetEntriesRequestingChange), []string{"Admin"})
     router.Handle("/admin/entry", getEntriesRequestingChanges).Methods("GET")
-	router.HandleFunc("/admin/report/{username}", controllers.GenerateAccountantExcelReport).Methods("GET")
+	
 	getUnconfirmedEntryRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(controllers.GetUnconfirmedEntries), []string{"Admin"})
     router.Handle("/admin/unconfirmed_entry", getUnconfirmedEntryRoute).Methods("GET")
-	confirmedEntryRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(controllers.ApproveEntry), []string{"Admin"})
-    router.Handle("/admin/entry/{entryId}", confirmedEntryRoute).Methods("PUT")
-	requestEntryChangeRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(controllers.RequestEntryChange), []string{"Admin"})
-    router.Handle("/admin/entry/{entryId}", requestEntryChangeRoute).Methods("POST")
+
+	// Admin/Manager
+	confirmedEntryRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userRoles := r.Context().Value("userRoles").([]string)
+		if middleware.IsAllowedAccess(userRoles, []string{"Admin", "Manager"}) {
+			controllers.ApproveEntry(w, r)
+		} else {
+			http.Error(w, "Not Allowed", http.StatusForbidden)
+		}
+	}), []string{"Admin", "Manager", "Accountant"})
+	router.Handle("/entry/{entryId}", confirmedEntryRoute).Methods("PUT")
+
+	
+	// Admin/Manager
+	requestEntryChangeRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userRoles := r.Context().Value("userRoles").([]string)
+		if middleware.IsAllowedAccess(userRoles, []string{"Admin", "Manager"}) {
+			controllers.RequestEntryChange(w, r)
+		} else {
+			http.Error(w, "Not Allowed", http.StatusForbidden)
+		}
+	}), []string{"Admin", "Manager", "Accountant"})
+	router.Handle("/entry/{entryId}", requestEntryChangeRoute).Methods("POST")
+	
+	//Admin alone
 	makeEntryChangeRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(controllers.MakeChangetoEntry), []string{"Admin"})
     router.Handle("/admin/entry_change/{entryId}", makeEntryChangeRoute).Methods("PUT")
 
-	router.HandleFunc("/admin/report/{username}", controllers.GenerateAccountantExcelReport).Methods("GET")
-	router.HandleFunc("/admin/report", controllers.GenerateAllAccountantExcelReport).Methods("GET")
+	combineAccessUsernameReportRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userRoles := r.Context().Value("userRoles").([]string)
+		if middleware.IsAllowedAccess(userRoles, []string{"Admin", "Manager"}) {
+			controllers.GenerateAccountantExcelReport(w, r)
+		} else {
+			http.Error(w, "Not Allowed", http.StatusForbidden)
+		}
+	}), []string{"Admin", "Manager", "Accountant"})
+	router.Handle("/report/{username}", combineAccessUsernameReportRoute).Methods("GET")
+
+
+	combineReportRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userRoles := r.Context().Value("userRoles").([]string)
+		if middleware.IsAllowedAccess(userRoles, []string{"Admin", "Manager"}) {
+			controllers.GenerateAllAccountantExcelReport(w, r)
+		} else {
+			http.Error(w, "Not Allowed", http.StatusForbidden)
+		}
+	}), []string{"Admin", "Manager", "Accountant"})
+	router.Handle("/report", combineReportRoute).Methods("GET")
+
+
+	combinedAccessAllEntriesRoute := middleware.RoleBasedJWTMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userRoles := r.Context().Value("userRoles").([]string)
+		if middleware.IsAllowedAccess(userRoles, []string{"Admin", "Manager"}) {
+			controllers.GetAllEntries(w, r)
+		} else {
+			http.Error(w, "Not Allowed", http.StatusForbidden)
+		}
+	}), []string{"Admin", "Manager", "Accountant"})
+	router.Handle("/entries", combinedAccessAllEntriesRoute).Methods("GET")
 
 
 
